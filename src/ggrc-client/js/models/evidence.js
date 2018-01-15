75c044df52f5eaf18c01b917ad23660e73e2c6c3
Artsiom Valchanin <volchanin.dev@gmail.com>
2018-01-15 13:27:12 +0300

    Add Evidence type

---
+++ src/ggrc-client/js/models/evidence.js
@@ -0,0 +1,76 @@
+/*
+  Copyright (C) 2017 Google Inc.
+  Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+*/
+import {getRole} from '../plugins/utils/acl-utils';
+
+const getAccessControlList = ()=> {
+  let adminRole = getRole('Evidence', 'Admin');
+
+  return adminRole ? [{
+    ac_role_id: adminRole.id,
+    person: {type: 'Person', id: GGRC.current_user.id},
+  }] : [];
+};
+
+can.Model.Cacheable('CMS.Models.Evidence', {
+  root_object: 'evidence',
+  root_collection: 'evidences',
+  title_singular: 'Evidence',
+  title_plural: 'Evidences',
+  category: 'governance',
+  findAll: 'GET /api/evidences',
+  findOne: 'GET /api/evidences/{id}',
+  create: 'POST /api/evidences',
+  update: 'PUT /api/evidences/{id}',
+  destroy: 'DELETE /api/evidences/{id}',
+  mixins: [
+    'accessControlList',
+    'ca_update',
+  ],
+  attributes: {
+    context: 'CMS.Models.Context.stub',
+  },
+  isRoleable: true,
+  statuses: [
+    'Active',
+    'Deprecated',
+  ],
+  kinds: [
+    {title: 'File', value: 'FILE'},
+    {title: 'URL', value: 'URL'},
+    {title: 'Refernce URL', value: 'REFERENCE_URL'},
+  ],
+  defaults: {
+    access_control_list: getAccessControlList(),
+    kind: 'FILE',
+    status: 'Active',
+  },
+  tree_view_options: {
+    attr_view: GGRC.mustache_path + '/evidences/tree-item-attr.mustache',
+    display_attr_names: [
+      'title',
+      'status',
+      'updated_at',
+    ],
+    attr_list: [
+      {attr_title: 'Title', attr_name: 'title'},
+      {attr_title: 'State', attr_name: 'status'},
+      {attr_title: 'Type', attr_name: 'kind'},
+      {attr_title: 'Last Updated By', attr_name: 'modified_by'},
+      {attr_title: 'Last Updated', attr_name: 'updated_at'},
+      {attr_title: 'Last Deprecated Date', attr_name: 'end_date'},
+      {attr_title: 'Archived', attr_name: 'archived'},
+    ],
+  },
+  init() {
+    this.validateNonBlank('title');
+    this._super(...arguments);
+  },
+}, {
+  kindTitle() {
+    let value = this.attr('kind');
+    let title = _.findWhere(this.class.kinds, {value}).title;
+    return title;
+  },
+});
