fb70249861f28e832258d816b116df4bb32af0f5
Aleh Natseuski <natsevsky@gmail.com>
2018-02-05 15:59:46 +0300

    Update GDrive integration tests

--- test/integration/ggrc_gdrive_integration/test_gdrive_auth.py
+++ test/integration/ggrc_gdrive_integration/test_gdrive_auth.py
@@ -3,7 +3,6 @@
 
 """Unit tests for Snapshot block converter class."""
 
-import urllib
 import mock
 import flask
 
@@ -12,22 +11,70 @@ import ggrc.gdrive as gi
 
 
 class TestAuthorizationFlow(TestCase):
-  """Unit tests for gDrive authorization flow."""
+  """Tests for gDrive authorization flow."""
+  # pylint: disable=invalid-name
 
-  # flake8: noqa pylint: disable=no-self-use
-  def test_gdrive_authorization(self):
-    """Test authorization routines work correctly"""
-    line = "http://some_test_redirect_url/export?model_type=Assessment&relevant_type=Person&relevant_id=150"
+  def test_gdrive_authorization_step_1(self):
+    """Test step 1 authorization
+
+    After the 1st step, we need to have a 'state' from google oauth provider
+    also, we should store redirect URL in flask.session to use it after
+    successful authorization.
+    """
+    line = "http://some_test_redirect_url/export?model_type=Assessment"
     flask.request.url = line
-    redirect = gi.authorize_app()
-    assert "state=" + urllib.quote_plus(line) in redirect.location
+    gi.authorize_gdrive()
+    self.assertEquals(flask.session['ggrc_view_to_redirect'], line)
+    self.assertTrue(flask.session['state'])
 
-    with mock.patch("ggrc.gdrive.client.OAuth2WebServerFlow") as mocked_flow:
-      # after the first step:
-      code = "1234567890"
-      flask.request.args = {"code": code}
-      # after the first step state is already in request.args
-      flask.request.args.update({"state": line})
-      gi.authorize_app()
-      # assert step2 called
-      mocked_flow.return_value.step2_exchange.assert_called_once_with(code)
+  def test_gdrive_authorization_step_1_self_redirect(self):
+    """Test step 1 authorization self redirect
+
+    In case of call '/authorize_gdrive' directly we need to update redirect URL
+    in pointing to host_url to prevent stuck with the self-redirect.
+    """
+    flask.request.path = '/authorize_gdrive'
+    flask.request.host_url = 'localhost:8080'
+    gi.authorize_gdrive()
+    self.assertEquals(flask.session['ggrc_view_to_redirect'], 'localhost:8080')
+    self.assertTrue(flask.session['state'])
+
+  def test_gdrive_authorization_step_2(self):
+    """Test step 2 authorization.
+
+    At step 2 we should get credentials from Google OAuth provider and
+    store them in 'flask.session'
+    We should clean flask.session['state'] and redirect to saved URL.
+    """
+    dummy_redirect_url = 'http://some_test_redirect_url'
+    flask.session['ggrc_view_to_redirect'] = dummy_redirect_url
+    flask.session['state'] = '12345'
+
+    with mock.patch('google_auth_oauthlib.flow.Flow') as mocked_flow:
+      dummy_credentials = mock.MagicMock()
+
+      dummy_credentials.token = 'super secret token'
+      dummy_credentials.refresh_token = 'super secret refresh_token'
+      dummy_credentials.token_uri = 'http://some_token_uri'
+      dummy_credentials.client_id = 'c_id'
+      dummy_credentials.client_secret = 'c_secret'
+      dummy_credentials.scopes = ['scope1']
+
+      dummy_flow = mock.MagicMock()
+      dummy_flow.credentials = dummy_credentials
+
+      mocked_flow.from_client_config.return_value = dummy_flow
+      redirect = gi.authorize()
+
+    expected_dict_cred = {
+        'token': dummy_credentials.token,
+        'refresh_token': dummy_credentials.refresh_token,
+        'token_uri': dummy_credentials.token_uri,
+        'client_id': dummy_credentials.client_id,
+        'client_secret': dummy_credentials.client_secret,
+        'scopes': dummy_credentials.scopes,
+
+    }
+    self.assertEquals(flask.session['credentials'], expected_dict_cred)
+    self.assertTrue('state' not in flask.session)
+    self.assertEquals(redirect.location, dummy_redirect_url)
